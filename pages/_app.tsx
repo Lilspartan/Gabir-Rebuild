6c0ec1ab7f3df65d0d014719f515d28b77b68a76
Gabe Krahulik <gabekrahulik@gmail.com>
2023-05-01 09:41:17 -0700

    fix GA?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,26 @@
 import '../styles/globals.css';
 import 'movement.css';
 import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-4G44T1DDKE"/>
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-4G44T1DDKE', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+      />
       <Component {...pageProps} />
       <Analytics />
     </>
