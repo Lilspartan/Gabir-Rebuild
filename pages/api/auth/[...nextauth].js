500002069867e49037b89b86aeafafee9bdd03bb
Lilspartan <gabekrahulik@gmail.com>
2023-06-07 23:04:46 -0700

    auth secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -37,6 +37,7 @@ export const authOptions = {
     logo: "https://gabirmotors.com/main.png",
     buttonText: "#ffffff"
   },
+  secret: "gabirmotorsportsistheworldspremierepretendmotorsportsorganization",
   callbacks: {
     async jwt({ token, account }) {
       // Persist the OAuth access_token to the token right after signin
